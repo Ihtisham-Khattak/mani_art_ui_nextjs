ca579182546effef2ad8a73de023c2232a8369fc
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-07-13 18:03:34 +0500

    all changes completed

--- pages/components/Banner/index.tsx
+++ pages/components/Banner/index.tsx
@@ -1,13 +1,13 @@
 const Banner = () => {
   return (
-    <div className='mx-auto my-10 h-screen  sm:py-10 px-6 lg:px-8 bg-[url("/images/gallery/Banner.jpg")] bg-center bg-cover bg-no-repeat'>
+    <div className='mx-auto my-10 h-screen  sm:py-10 px-6 lg:px-8 bg-[url("/images/gallery/Banner.jpg")] bg-center bg-contain bg-no-repeat'>
       <div className="my-16">
         {/* COLUMN-1 */}
 
-        <div className="mx-auto sm:mx-0">
-          <div className="w-[50%] h-36 mx-auto bg-[#28312C] rounded-lg">
-            <div className="py-4 text-center translate-y-5 lg:text-start">
-              <h2 className="text-white font-bold text-3xl text-center">
+        <div className="mx-auto sm:mx-0 flex justify-center items-center">
+          <div className="w-full sm:w-1/2 h-36 mx-auto sm:mx-auto flex items-center justify-center bg-[#28312C] rounded-lg">
+            <div className="py-4 text-center sm:text-left">
+              <h2 className="text-white text-center font-bold text-2xl sm:text-3xl">
                 Mani Art Studio bring your <br /> ideas to life.
               </h2>
             </div>
